82b9d383ab3caabe5d5a9ec83e0302b1db50035e
Zachary Seguin <zachary.seguin@canada.ca>
2020-01-07 14:55:12 +0000

    feat(outputs): Add output for release revision

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "helm_namespace" {
   value = "${var.helm_namespace}"
 }
 
+output "release_revision" {
+  value = "${helm_release.vault.metadata.0.revision}"
+}
+
 # Part of a hack for module-to-module dependencies.
 # https://github.com/hashicorp/terraform/issues/1178#issuecomment-449158607
 # and
